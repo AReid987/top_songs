45cc5514cdc7d2a2d197663081666e75eb1a99b5
Antonio Reid <read.musik@gmail.com>
2018-02-22 02:15:14 +0000

    stubbed list_songs method

--- lib/top_songs/cli.rb
+++ lib/top_songs/cli.rb
@@ -1,7 +1,15 @@
 class TopSongs::CLI
 
   def call 
-    puts "Today's top songs"
+    puts "Today's top songs:"
+    list_songs
   end
 
+  def list_songs
+    puts "1. Drake"
+    puts "2. Ed Sheeran"
+    puts "3. Bruno Mars & Cardi B."
+    puts "4. Camila Cabello"
+    puts "5. Post Malone Featuring 21 Savage"
+  end
 end
