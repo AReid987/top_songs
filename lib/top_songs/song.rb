d06a9366fd170a24446dfbb257f3994e61b80f59
Antonio Reid <read.musik@gmail.com>
2018-02-24 02:09:09 +0000

    added artist attribute to song objects

--- lib/top_songs/song.rb
+++ lib/top_songs/song.rb
@@ -7,7 +7,7 @@ class TopSongs::Song
   def self.create_songs(song)
       new_song = TopSongs::Song.new
       new_song.name = song.css("h2.chart-row__song").text #=> name
-      #new_song.artist = song.css("a.chart-row__artist")[i].text.strip #=> artist
+      new_song.artist = song.css(".chart-row__artist").text.strip #=> artist
       new_song.number = song.css("span.chart-row__current-week").text# => current rank
       new_song.last_week = song.css("span.chart-row__last-week").text #=> last week
 
